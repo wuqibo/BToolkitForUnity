5188e2cdf7fa8f4147a60b518a77006270e35876
wuqibo <wqb55@qq.com>
2019-05-21 17:31:17 +0800

    no description

---
+++ TimeHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BToolkit
+{
+    public class TimeHelper
+    {
+
+        /// <summary>
+        /// 获取当前时间戳（秒）
+        /// </summary>
+        public static long GetCurrTimeStamp()
+        {
+            //柏林时间DateTime.UtcNow
+            TimeSpan ts = DateTime.Now - new DateTime(1970, 1, 1, 0, 0, 0, 0);
+            return Convert.ToInt64(ts.TotalSeconds);
+        }
+
+        /// <summary>
+        /// 获取当前时间（格式：2000-01-01 01:01:01）
+        /// </summary>
+        public static string GetCurrTimeString()
+        {
+            //使用本地时间
+            DateTime dt = DateTime.Now;
+            return dt.ToString("yyyy-MM-dd HH:mm:ss");
+        }
+
+    }
+}
